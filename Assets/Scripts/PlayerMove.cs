44abd52b2f7fd641cf97e27b30d3c254eee5ae47
Sophie Penny <512894@escg.ac.uk>
2020-10-12 12:21:16 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    int timer;
+    int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        direction = 2;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer ++;
+        //Debug.Log(timer);
+        rb.velocity = new Vector2(direction, 0);
+        if (timer > 200)
+        {
+            direction = -direction;
+            timer = 0;
+        }
+    }
+}
+
+
+
---
+++ Assets/Scripts/playermove1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class playermove1 : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 Velocity = rb.velocity;
+        if (Input.GetKey("space"))
+        {
+            Velocity.y = 12;
+        }
+ 
+        if (Input.GetKey("a"))
+        {
+            Velocity.x = -9;
+        }
+        if (Input.GetKey("d"))
+        {
+            Velocity.x = 9;
+        }
+        Velocity.y = -9;
+        rb.velocity = Velocity;
+    }
+}
