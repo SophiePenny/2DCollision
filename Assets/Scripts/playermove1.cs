6c60edf6176d15b9c523fd5e7d141e6b8008db37
SophiePenny <512894@chrome.escg.ac.uk>
2020-10-13 13:34:14 +0100

    jumping works properly

--- Assets/Scripts/playermove1.cs
+++ Assets/Scripts/playermove1.cs
@@ -17,18 +17,21 @@ public class playermove1 : MonoBehaviour
         Vector2 Velocity = rb.velocity;
         if (Input.GetKey("space"))
         {
-            Velocity.y = 12;
+            if (Velocity.y == 0)
+            {
+                Velocity.y = 11;
+            }
+
         }
  
         if (Input.GetKey("a"))
         {
-            Velocity.x = -9;
+            Velocity.x = -5;
         }
         if (Input.GetKey("d"))
         {
-            Velocity.x = 9;
+            Velocity.x = 5;
         }
-        Velocity.y = -9;
         rb.velocity = Velocity;
     }
 }
